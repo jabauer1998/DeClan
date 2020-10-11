c5a07420fdabeddfd6f18ff8c64a63627ce454de
h20man13 <jacobbauer_2021@depauw.edu>
2020-10-10 21:45:08 -0400

    finished project 3 non optional

--- DeCLan/src/main/java/edu/depauw/declan/main/MyInterpreter.java
+++ DeCLan/src/main/java/edu/depauw/declan/main/MyInterpreter.java
@@ -11,41 +11,55 @@ import edu.depauw.declan.common.ast.NumValue;
 import edu.depauw.declan.common.ast.ProcedureCall;
 import edu.depauw.declan.common.ast.Program;
 import edu.depauw.declan.common.ast.UnaryOperation;
+import edu.depauw.declan.common.ast.Statement;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import static edu.depauw.declan.common.MyIO.*;
 
 public class MyInterpreter implements ASTVisitor, ExpressionVisitor<Integer> {
 	private ErrorLog errorLog;
+        private Map<String, String> environment;
 	// TODO declare any data structures needed by the interpreter
 	
 	public MyInterpreter(ErrorLog errorLog) {
 		this.errorLog = errorLog;
-		// TODO initialize any data structures needed by the interpreter
+		this.environment = new HashMap<>();
 	}
 
 	@Override
 	public void visit(Program program) {
-		// TODO Auto-generated method stub
-
+	        for (ConstDeclaration constDecl : program.getConstDecls()) {
+			constDecl.accept(this);
+		}
+		for (Statement statement : program.getStatements()) {
+			statement.accept(this);
+		}
 	}
 
 	@Override
 	public void visit(ConstDeclaration constDecl) {
-		// TODO Auto-generated method stub
-
+	        Identifier id = constDecl.getIdentifier();
+		NumValue num = constDecl.getNumber();
+		environment.put(id.getLexeme(), num.getLexeme());
 	}
 
 	@Override
 	public void visit(ProcedureCall procedureCall) {
-		// TODO Auto-generated method stub
-
+		if (procedureCall.getProcedureName().getLexeme().equals("PrintInt")) {
+			int value = procedureCall.getArgument().acceptResult(this);
+			OUT("" + value);
+		} else {
+			// Ignore all other procedure calls
+		}
 	}
-
-	@Override
+        @Override
 	public void visit(EmptyStatement emptyStatement) {
 		// TODO Auto-generated method stub
 
 	}
-
-	@Override
+        @Override
 	public void visit(UnaryOperation unaryOperation) {
 		// Not used
 	}
@@ -64,29 +78,46 @@ public class MyInterpreter implements ASTVisitor, ExpressionVisitor<Integer> {
 	public void visit(Identifier identifier) {
 		// Not used
 	}
-
+    
 	@Override
 	public Integer visitResult(BinaryOperation binaryOperation) {
-		// TODO Auto-generated method stub
+		Integer leftvalue = binaryOperation.getLeft().acceptResult(this);
+		Integer rightvalue = binaryOperation.getRight().acceptResult(this);
+		switch (binaryOperation.getOperator()) {
+		case PLUS:
+		    return leftvalue + rightvalue;
+		case MINUS:
+		    return leftvalue - rightvalue;
+		case TIMES:
+		    return leftvalue * rightvalue;
+		case DIV:
+		    return leftvalue / rightvalue;
+		case MOD:
+		    return leftvalue % rightvalue;
+		}
 		return null;
 	}
 
 	@Override
 	public Integer visitResult(UnaryOperation unaryOperation) {
-		// TODO Auto-generated method stub
+		Integer value = unaryOperation.getExpression().acceptResult(this);
+		switch (unaryOperation.getOperator()) {
+		case PLUS:
+		    return value;
+		case MINUS:
+		    return -value;
+		}
 		return null;
 	}
 
 	@Override
 	public Integer visitResult(Identifier identifier) {
-		// TODO Auto-generated method stub
-		return null;
+		return Integer.parseInt(environment.getOrDefault(identifier.getLexeme(), "0"));
 	}
 
 	@Override
 	public Integer visitResult(NumValue numValue) {
-		// TODO Auto-generated method stub
-		return null;
+		return Integer.parseInt(numValue.getLexeme());
 	}
 
 }
