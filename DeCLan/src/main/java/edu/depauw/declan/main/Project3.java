b3df38bdfd8c4a109ed7d1129f7e19f8aeac67e8
h20man13 <jacobbauer_2021@depauw.edu>
2020-10-12 20:45:08 -0400

    Added components to language to allow for floating point values

--- DeCLan/src/main/java/edu/depauw/declan/main/Project3.java
+++ DeCLan/src/main/java/edu/depauw/declan/main/Project3.java
@@ -18,17 +18,20 @@ import edu.depauw.declan.common.ast.Program;
 public class Project3 {
 	public static void main(String[] args) {
 		String demoSource =
-				  "CONST six = 6; seven = 7;\n"
+				  "CONST six = 6; seven = 7; bignine = 9.E-3;\n"
 				+ "BEGIN\n"
 				+ "  PrintInt(seven - six);\n"
 				+ "  PrintInt(2 * (six + seven) MOD six);\n"
 				+ "  PrintInt(six - seven DIV 2);\n"
 				+ "  PrintInt(six * seven);\n"
+		                + "  PrintDouble(1.E1);\n"
+		                + "  PrintDouble(1.87);\n"
+		                + "  PrintDouble(bignine);\n"
 				+ "END.\n";
 
 		Properties props = new Properties();
-		props.setProperty("useModelLexer", "true");
-		props.setProperty("useModelParser", "true");
+		props.setProperty("useModelLexer", "false");
+		props.setProperty("useModelParser", "false");
 		props.setProperty("useModelInterpreter", "false");
 		props.setProperty("sourceFile", "");
 		props.setProperty("demoSource", demoSource);
