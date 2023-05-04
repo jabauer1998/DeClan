69f22845e629260e1a48114ff58bbb0e524aae44
H20man13 <jabauer.1998@gmail.com>
2023-05-04 11:37:32 -0700

    Added more test coverage for the Declan Parser

---
+++ DeClan/src/test/java/io/github/H20man13/DeClan/MyParserBasicTest.java
@@ -0,0 +1,75 @@
+package io.github.H20man13.DeClan;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.junit.Test;
+
+import edu.depauw.declan.common.ErrorLog;
+import edu.depauw.declan.common.Source;
+import edu.depauw.declan.common.ast.Program;
+import io.github.H20man13.DeClan.common.ReaderSource;
+import io.github.H20man13.DeClan.main.MyDeClanLexer;
+import io.github.H20man13.DeClan.main.MyDeClanParser;
+
+public class MyParserBasicTest {
+    public void runParserOnSource(String sourcePath){
+        File sourceFile = new File(sourcePath);
+        assertTrue("Error file " + sourceFile.getAbsolutePath() + " doesnt exist!!", sourceFile.exists());
+
+        
+        try {
+            FileReader reader = new FileReader(sourceFile);
+            Source mySource = new ReaderSource(reader);
+            ErrorLog errLog = new ErrorLog();
+            MyDeClanLexer lexer = new MyDeClanLexer(mySource, errLog);
+            MyDeClanParser parser = new MyDeClanParser(lexer, errLog);
+            Program prog = parser.parseProgram();
+        } catch(Exception exp){
+            assertTrue(exp.toString(), false);
+        }
+    }
+
+    @Test
+    public void testConversions(){
+        runParserOnSource("test_source/conversions.dcl");
+    }
+
+    @Test
+    public void testExpressions(){
+        runParserOnSource("test_source/expressions.dcl");
+    }
+
+    @Test
+    public void testLoops(){
+        runParserOnSource("test_source/loops.dcl");
+    }
+
+    @Test
+    public void testSample(){
+        runParserOnSource("test_source/sample.dcl");
+    }
+
+    @Test
+    public void testTest(){
+        runParserOnSource("test_source/test.dcl");
+    }
+
+    @Test
+    public void testTest2(){
+        runParserOnSource("test_source/test2.dcl");
+    }
+
+    @Test
+    public void testTest3(){
+        runParserOnSource("test_source/test3.dcl");
+    }
+
+    @Test
+    public void testTest4(){
+        runParserOnSource("test_source/test4.dcl");
+    }
+    
+}
