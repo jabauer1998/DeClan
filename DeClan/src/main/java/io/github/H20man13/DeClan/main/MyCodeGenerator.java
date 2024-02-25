d819f778db7959909f8a13c165b8d48dcfae9e90
H20man13 <jabauer.1998@gmail.com>
2024-02-24 21:05:20 -0800

    Fixed issue in MyCodeGenerator

--- DeClan/src/main/java/io/github/H20man13/DeClan/main/MyCodeGenerator.java
+++ DeClan/src/main/java/io/github/H20man13/DeClan/main/MyCodeGenerator.java
@@ -2177,7 +2177,7 @@ public class MyCodeGenerator {
 
                     String reg = rGen.getReg(sourceDest.value.toString(), procICode);
                     //TODO finish later 
-                    cGen.addInstruction("LDR " + reg + ", " + sourceDest.source);
+                    cGen.addInstruction("LDR " + reg + ", " + sourceDest.place);
                     cGen.addInstruction("STR " + reg +  ", [R13,-" + offReg + "]");
                 }
                 cGen.addInstruction("BL " + procICode.pname);
@@ -9885,22 +9885,22 @@ public class MyCodeGenerator {
                 int toAllocateToStack = totalReturnStackLengthInBytes + 4;
                 int toPlaceReturnAddressOnStack = toAllocateToStack;
                 int offset = toAllocateToStack;
-                for(Tuple<String, String> param: procICode.params){
+                for(Assign param: procICode.params){
                     offset -= 4;
-                    cGen.addVariable(param.dest, VariableLength.WORD, offset);
+                    cGen.addVariable(param.place, VariableLength.WORD, offset);
                 }
                 
                 cGen.addInstruction("ADD R13, R13, #" + toAllocateToStack);
                 cGen.addInstruction("STR R14, [R13, #-"+toPlaceReturnAddressOnStack+"]");
                 for(int x = 0; x < totalLength; x++){
-                    Tuple<String, String> sourceDest = procICode.params.get(x);
+                    Assign sourceDest = procICode.params.get(x);
 
                     String offSetRegister = iGen.genNext();
                     String offReg = rGen.getTempReg(offSetRegister, procICode);
-                    cGen.addInstruction("LDR " + offReg + ", " + sourceDest.dest);
+                    cGen.addInstruction("LDR " + offReg + ", " + sourceDest.place);
 
-                    String reg = rGen.getReg(sourceDest.source, procICode); 
-                    cGen.addInstruction("LDR " + reg + ", " + sourceDest.source);
+                    String reg = rGen.getReg(sourceDest.value.toString(), procICode); 
+                    cGen.addInstruction("LDR " + reg + ", " + sourceDest.value.toString());
                     cGen.addInstruction("STR " + reg +  ", [R13,-" + offReg + "]");
                 }
                 cGen.addInstruction("BL " + procICode.pname);
