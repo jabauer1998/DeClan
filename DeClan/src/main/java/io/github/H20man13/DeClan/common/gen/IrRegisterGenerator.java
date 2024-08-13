591b59430b425da48c4c6e7fd5a1fb7bbdb3a334
H20man13 <jabauer.1998@gmail.com>
2024-08-12 21:06:02 -0700

    Ran the updater script and fixed issue with the IrRegisterGenerator generating previously generated registers

--- DeClan/src/main/java/io/github/H20man13/DeClan/common/gen/IrRegisterGenerator.java
+++ DeClan/src/main/java/io/github/H20man13/DeClan/common/gen/IrRegisterGenerator.java
@@ -23,42 +23,43 @@ public class IrRegisterGenerator implements Generator {
 		if(firstRound){
 			String awnser = "" + currentLetter;
 			if(currentLetter != endLetter){
-			currentLetter++;
+				currentLetter++;
 			} else if(currentLetter == 'z'){
-			startLetter = Character.toUpperCase(startLetter);
-			endLetter = Character.toUpperCase(endLetter);
-			currentLetter = startLetter;
+				startLetter = Character.toUpperCase(startLetter);
+				endLetter = Character.toUpperCase(endLetter);
+				currentLetter = startLetter;
 			} else if (currentLetter == 'Z'){
-			startLetter = Character.toLowerCase(startLetter);
-			endLetter = Character.toLowerCase(endLetter);
-			currentLetter = startLetter;
-			firstRound = false;
+				startLetter = Character.toLowerCase(startLetter);
+				endLetter = Character.toLowerCase(endLetter);
+				currentLetter = startLetter;
+				firstRound = false;
 			}
 			return awnser;
 		} else {
 			String awnser = "" + currentLetter + currentNumber;
 			if(currentNumber != endNumber){
-			currentNumber++;
+				currentNumber++;
 			} else {
-			if(currentLetter != endLetter){
-				currentLetter++;
-				currentNumber = startNumber;
-			} else if (endLetter == 'z') {
-				startLetter = Character.toUpperCase(startLetter);
-				endLetter = Character.toUpperCase(endLetter);
-				currentLetter = startLetter;
-				currentNumber = startNumber;
-			} else if (endLetter == 'Z') {
-				startLetter = Character.toLowerCase(startLetter);
-				endLetter = Character.toLowerCase(endLetter);
-				currentLetter = startLetter;
-				if(startNumber == 0){
-				startNumber = 10;
-				} else {
-				startNumber *= 10;
+				if(currentLetter != endLetter){
+					currentLetter++;
+					currentNumber = startNumber;
+				} else if (endLetter == 'z') {
+					startLetter = Character.toUpperCase(startLetter);
+					endLetter = Character.toUpperCase(endLetter);
+					currentLetter = startLetter;
+					currentNumber = startNumber;
+				} else if (endLetter == 'Z') {
+					startLetter = Character.toLowerCase(startLetter);
+					endLetter = Character.toLowerCase(endLetter);
+					currentLetter = startLetter;
+					if(startNumber == 0){
+						startNumber = 10;
+					} else {
+						startNumber *= 10;
+					}
+					currentNumber = startNumber;
+					endNumber = endNumber * 10 + 9;
 				}
-				endNumber = endNumber * 10 + 9;
-			}
 			}
 			return awnser;
 		}
