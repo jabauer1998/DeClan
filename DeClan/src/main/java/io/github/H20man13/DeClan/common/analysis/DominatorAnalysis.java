8499f54a7bd5b6c11fc0d6f59cb1dc02a38d1893
H20man13 <jabauer.1998@gmail.com>
2024-09-15 19:25:12 -0700

    Added a Dominator Analysis

---
+++ DeClan/src/main/java/io/github/H20man13/DeClan/common/analysis/DominatorAnalysis.java
@@ -0,0 +1,21 @@
+package io.github.H20man13.DeClan.common.analysis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import io.github.H20man13.DeClan.common.flow.FlowGraph;
+import io.github.H20man13.DeClan.common.flow.FlowGraphNode;
+
+public class DominatorAnalysis extends BasicBlockAnalysis<FlowGraphNode> {
+	public DominatorAnalysis(FlowGraph flowGraph) {
+		super(flowGraph, Direction.FORWARDS, Meet.INTERSECTION);
+	}
+
+	@Override
+	public Set<FlowGraphNode> transferFunction(FlowGraphNode instr, Set<FlowGraphNode> inputSet) {
+		Set<FlowGraphNode> toRet = new HashSet<FlowGraphNode>();
+		toRet.addAll(inputSet);
+		toRet.add(instr);
+		return toRet;
+	}
+}
